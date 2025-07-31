e8f3578ee546d7dfc3b952f4619d53b466ad6ef5
Pedro Vasconcelos <pedrohenriquedeoliveiravasconc@gmail.com>
2025-07-31 14:45:20 -0300

    Add files via upload

---
+++ exe08a.py
@@ -0,0 +1,4 @@
+nome = input('Olá, muito prazer tê-lo aqui no nosso grupo de Cálculos')
+n1 = int(input('Para darmos início, me conte, quantos metros você mediu?'))
+print('Poxa é super simples, {} metros equivalem à {} centímeros. Porém equivalem à {} milímetros.'.format(n1, n1*100, n1*1000))
+
